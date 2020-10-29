0f499a04789ab1da97f4b9a0e12f240506f46140
Matt <matthewportermcdowell@gmail.com>
2020-10-29 01:27:30 -0400

    Trying to add user input to create books I think both of the saveBook methods are different ways to create the book object

--- src/main/java/com/tutorialtwo/tutorialtwo/controller/TutorialController.java
+++ src/main/java/com/tutorialtwo/tutorialtwo/controller/TutorialController.java
@@ -1,9 +1,13 @@
 package com.tutorialtwo.tutorialtwo.controller;
 
+import com.tutorialtwo.tutorialtwo.domain.Book;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TutorialController {
@@ -14,5 +18,24 @@ public class TutorialController {
     public String homePage(Model model){
         model.addAttribute("appName", appName);
         return "home";
+    }
+    @GetMapping("/createBook")
+    public String saveBook(Model model){
+        try {
+//            book.save(book);
+            model.addAttribute("book", new Book());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return  "createBook";
+//        https://stackoverflow.com/questions/40899494/how-to-get-input-values-from-spring-boot-thyme-leaf-to-java-class
+    }
+    @PostMapping("/createBook")
+    public String saveBook(@ModelAttribute("book") Book book) {
+
+        book.getTitle();
+        book.getAuthor();
+        return "redirect:someOtherPage";
     }
 }
