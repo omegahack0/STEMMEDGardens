bb0fb81c43627006b87de2aa3c22950762a6f98f
Matt <matthewportermcdowell@gmail.com>
2020-11-07 20:59:30 -0500

    Adding a webform so user can input their zip

--- src/main/java/com/tutorialtwo/tutorialtwo/controller/TutorialController.java
+++ src/main/java/com/tutorialtwo/tutorialtwo/controller/TutorialController.java
@@ -1,9 +1,12 @@
 package com.tutorialtwo.tutorialtwo.controller;
 
+import com.tutorialtwo.tutorialtwo.userZipcode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TutorialController {
@@ -14,5 +17,18 @@ public class TutorialController {
     public String homePage(Model model){
         model.addAttribute("appName", appName);
         return "home";
+    }
+    @GetMapping("/GetUserZipcode")
+    public String zipcodeForm(Model model) {
+        model.addAttribute("GetUserZipcode", new userZipcode());
+        //need to add try catch statement to verify user input
+        return "GetUserZipcode";
+    }
+
+    @PostMapping("/GetUserZipcode")
+    public String zipcodeSubmit(@ModelAttribute userZipcode userZip, Model model) {
+        model.addAttribute("GetUserZipcode", userZip);
+        return "result";
+
     }
 }
---
+++ src/main/java/com/tutorialtwo/tutorialtwo/userZipcode.java
@@ -0,0 +1,13 @@
+package com.tutorialtwo.tutorialtwo;
+
+public class userZipcode {
+    public long zipcode;
+
+    public long getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(long zipcode) {
+        this.zipcode = zipcode;
+    }
+}
